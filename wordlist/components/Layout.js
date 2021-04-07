c4a32815e4acfdd7251739098fdf18d4afe6bb55
Raj <97rajmath@gmail.com>
2021-04-07 12:01:01 -0500

    fix the submodule issue

---
+++ wordlist/components/Layout.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Navbar,Nav,Form,FormControl,Button} from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Layout({ children }) {
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/about">About</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search Words" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+
+            {children}
+
+        </div>
+    )
+}
+
+export default Layout
---
+++ wordlist/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+
+  return (
+    <div className={styles.container}>
+        <h1>WordList Application</h1>
+    </div>
+  )
+}
